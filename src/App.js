57adc4570ed3e44cd92638d6c7328078ade6d29a
Paulina <paulina2651997@gmail.com>
2022-11-05 17:27:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'styles/GlobalStyle';
+import { colors } from 'styles/theme';
+
+const Wrapper = styled.div`
+  background-color: ${({ theme }) => theme.lightBlack};
+  color: ${({ theme }) => theme.lightGray};
+  min-height: 100vh;
+  min-width: 100vw;
+  overflow: hidden;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={colors}>
+      <GlobalStyle />
+      <Wrapper>
+        <p>dupka żonki</p>
+      </Wrapper>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/theme.js
@@ -0,0 +1,17 @@
+export const colors = {
+  lightBlack: '#353535',
+  midiumBlack: '#202020',
+  black: '#000000',
+  lightGray: '#D9D9D9',
+  gray: '#D6D6D6',
+  darkGray: '#333533',
+  lightYellow: '#FFEE32',
+  yellow: '#F5CB5C',
+  darkYellow: '#DDB703',
+  lightBlue: '#00B4D8',
+  blue: '#0096C7',
+  darkBlue: '#023E8A',
+  liaghtGreen: '#80B918',
+  green: '#55A630',
+  darkGreen: '#2B9348',
+};
