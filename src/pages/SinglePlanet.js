0a9832f29e3dd52161146438a577529e78dc0038
Paulina <paulina2651997@gmail.com>
2023-02-26 13:06:49 +0100

    add getSingleListData to single Planet

--- src/pages/SinglePlanet.js
+++ src/pages/SinglePlanet.js
@@ -2,14 +2,33 @@ import SingleElementWrapper from 'shared/SingleElementWrapper';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import Loader from '../components/Loader';
+import { get } from 'api/api';
 
 const SinglePlanet = () => {
+  const params = useParams();
+  const [planet, setPlanet] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getSingleListData = async () => {
+    setIsLoading(true);
+    const apiLink = `/planets/${params.idPlanet}`;
+    const planet = await get(apiLink);
+    setPlanet(planet);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getSingleListData();
+  }, []);
+
+  if (isLoading) return <Loader />;
   return (
-    <SingleElementWrapper title={'planet.name'} backButton='/planets'>
-      {/* <p>{`Climate: ${props.climate}`}</p> */}
-      {/* <p>{`Eye Color: ${props.eye_color}`}</p>
-      <p>{`Birth Year: ${props.birth_year}`}</p>
-      <p>{`Gender: ${props.gender}`}</p> */}
+    <SingleElementWrapper title={planet.name} backButton='/planets'>
+      <p>{`Climate: ${planet.climate}`}</p>
+      <p>{`Diameter: ${planet.diameter}`}</p>
+      <p>{`Gravity: ${planet.gravity}`}</p>
+      <p>{`Population: ~${planet.population}`}</p>
+      <p>{`Terrain: ${planet.terrain}`}</p>
     </SingleElementWrapper>
   );
 };
